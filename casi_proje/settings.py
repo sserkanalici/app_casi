fb3c2c3099cb8088f2329e69622ab3e38332b7e2
serkanalici <serkan.alici@outlook.com>
2024-07-26 01:37:42 +0300

    settings9

--- casi_proje/settings.py
+++ casi_proje/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import whitenoise
+import whitenoise.middleware
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    whitenoise.middleware.WhiteNoiseMiddleware
 ]
 
 ROOT_URLCONF = 'casi_proje.urls'
