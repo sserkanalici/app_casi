200dd18d60bb08c252f289eba58563f02400485a
serkanalici <serkan.alici@outlook.com>
2024-07-26 01:39:56 +0300

    settings9

--- casi_proje/settings.py
+++ casi_proje/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    whitenoise.middleware.WhiteNoiseMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'casi_proje.urls'
